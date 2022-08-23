6f7c4f7df02ffe17821621910c4772b7b656ac0f
jiveshshrivastav11 <83312246+jiveshshrivastav11@users.noreply.github.com>
2022-08-23 12:59:50 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -12,11 +12,13 @@ public class FirstTest {
     @Test
     public void OpenBrowser()  {
         WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        options.addArguments("disable-gpu");
-        driver = new ChromeDriver(options);
+        String driverpath = System.getProperty("user.dir") + "/Chromedriver/chromedriver";
+		System.setProperty("webdriver.chrome.driver", driverpath + ".exe");
+//         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+//         ChromeOptions options = new ChromeOptions();
+//         options.addArguments("headless");
+//         options.addArguments("disable-gpu");
+        driver = new ChromeDriver();
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
